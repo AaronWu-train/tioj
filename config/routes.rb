99c915634304bbbd3383196380e04fb64bde3186
ckeisc42nd_aaw <ckefgisc27th@gmail.com>
2023-09-02 10:23:47 +0800

    Revert "Finish registration batch operations"
    This reverts commit 4273150cc4ec13e1d30bc9996df9f90bc8af079b.

--- config/routes.rb
+++ config/routes.rb
@@ -55,11 +55,11 @@ Rails.application.routes.draw do
       resources :comments, except: [:index]
     end
 
-    resources :contest_registrations, only: [:index, :new, :create, :update, :destroy] do
+    resources :contest_registrations, only: [:index, :new, :create, :destroy] do
       collection do
         get 'batch_new'
         post 'batch_new', to: 'contest_registrations#batch_create'
-        post 'batch_op'
+        post 'batch_delete'
       end
     end
 
