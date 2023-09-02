e0450965825e165197e62a233f1ae07cbc990500
ckeisc42nd_aaw <ckefgisc27th@gmail.com>
2023-09-02 10:24:11 +0800

    Revert "Finish single contest session management"
    This reverts commit 301b242ee6c309cda9f4d89d17ae57a0c7986a11.

--- app/controllers/concerns/single_contest_authentication_concern.rb
+++ app/controllers/concerns/single_contest_authentication_concern.rb
@@ -9,29 +9,27 @@ module SingleContestAuthenticationConcern
 
   def current_user
     if @layout == :single_contest
-      user_id = session.dig(:single_contest, @contest.id, :user_id)
-      # TODO: store partial password for password change?
-      return nil if user_id.nil?
-      return User.find_by(id: user_id)
+      return nil
     end
     super
   end
 
   def user_signed_in?
+    if @layout == :single_contest
+      return false
+    end
     super
   end
 
   def user_session
     if @layout == :single_contest
-      return session.dig(:single_contest, @contest.id, :user_session)
+      return 'meow'
     end
     super
   end
 
   def authenticate_user!
     if @layout == :single_contest
-      return if user_signed_in?
-      redirect_to sign_in_single_contest_path(@contest)
     end
     super
   end
