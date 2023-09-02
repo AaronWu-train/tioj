f53621c283366ed50a7b6655f1ebf1f19720214c
ckeisc42nd_aaw <ckefgisc27th@gmail.com>
2023-09-02 10:24:01 +0800

    Revert "Rename to registration"
    This reverts commit 0af310c6e40276353d7d58b1a57d6d893b6be8da.

--- app/models/contest_user_joint.rb
+++ app/models/contest_user_joint.rb
@@ -1,6 +1,6 @@
 # == Schema Information
 #
-# Table name: registrations
+# Table name: contest_user_joints
 #
 #  id         :bigint           not null, primary key
 #  user_id    :bigint           not null
@@ -11,12 +11,12 @@
 #
 # Indexes
 #
-#  index_registrations_on_contest_id_and_approved  (contest_id,approved)
-#  index_registrations_on_contest_id_and_user_id   (contest_id,user_id) UNIQUE
-#  index_registrations_on_user_id_and_approved     (user_id,approved)
+#  index_contest_user_joints_on_contest_id_and_approved  (contest_id,approved)
+#  index_contest_user_joints_on_contest_id_and_user_id   (contest_id,user_id) UNIQUE
+#  index_contest_user_joints_on_user_id_and_approved     (user_id,approved)
 #
 
-class Registration < ApplicationRecord
+class ContestUserJoint < ApplicationRecord
   default_scope { order('id ASC') }
 
   belongs_to :contest
--- db/migrate/20230602023419_rename_registration_table.rb
+++
@@ -1,5 +0,0 @@
-class RenameRegistrationTable < ActiveRecord::Migration[7.0]
-  def change
-    rename_table :contest_user_joints, :registrations
-  end
-end
