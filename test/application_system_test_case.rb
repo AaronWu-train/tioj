e21659e46ce2e625f023cc00c5b957d4880ca7d6
ckeisc42nd_aaw <ckefgisc27th@gmail.com>
2023-09-02 10:24:31 +0800

    Revert "Add ranklist tests"
    This reverts commit eb06dff57d06f5c38ac30c6c15d5109935a5ed6e.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,14 +1,11 @@
 require "test_helper"
 
+Selenium::WebDriver.logger.output = 'tmp/selenium.log'
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome
   include SystemSignInHelper
 
   # removes noisy logs when launching tests
   Capybara.server = :puma, { Silent: true }
-
-  teardown do
-    # check JavaScript errors
-    assert_empty page.driver.browser.logs.get(:browser).select{|x| x.level == 'SEVERE'}
-  end
 end
