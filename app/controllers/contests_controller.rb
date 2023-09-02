86836aed35853013587a5d1f7a02ddcf0357af71
ckeisc42nd_aaw <ckefgisc27th@gmail.com>
2023-09-02 10:24:19 +0800

    Revert "Store subtask results for better performance"
    This reverts commit e88cfa3fd8bc8775937062b8f63510a81015a95d.

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -33,7 +33,8 @@ class ContestsController < ApplicationController
     else
       c_submissions = @contest.submissions
     end
-    c_submissions = c_submissions.includes(:submission_subtask_result) if @contest.type_ioi_new?
+    # TODO: Too slow on new IOI style
+    c_submissions = c_submissions.includes(:submission_testdata_results).includes(problem: [:subtasks, :testdata]) if @contest.type_ioi_new?
 
     freeze_start = (
         (current_user&.admin? && !params[:with_freeze]) || self_only ?
